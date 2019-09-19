110d3853df749cab0342887c15f5d3613666bd55
mariuszadams <37147820+mariuszadams@users.noreply.github.com>
2019-09-19 18:07:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-echo '<h1>PHP version: ' . phpversion();
-echo '<br/>';
+echo '<h1>PHP version: ' . phpversion() . '<br/>';
 echo  $_ENV['HOSTNAME'] .' @ '. $_SERVER['SERVER_ADDR'] .' >>> '. $_SERVER['HTTP_HOST'].'</h1>';
-if($_GET){
+if($_GET)
+{
   foreach($_ENV as $var => $val)
   {
     echo $var.'='.$val.'<br/>';
   }
 }
-?> 
+?>
